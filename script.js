cda26d85367215d92fe391cea0a517aa5bab3579
Lucas Miranda <lucasmiranda.da.silva8@gmail.com>
2024-07-15 21:49:55 -0300

    Project final

---
+++ script.js
@@ -0,0 +1,11 @@
+//Document = Ir no HTML
+//querySelector = busca o que eu quero
+let form = document.querySelector(".form")
+
+function mostrarForm() {
+  form.style.left = "50%"
+}
+
+function fecharForm() {
+  form.style.left = "-100%"
+}
